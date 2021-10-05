7c366d4c5451ea8e68c34b964223231e3adf3384
Tayse <tayse_rosa@yahoo.com.br>
2021-10-05 11:19:32 -0300

    React JS Desafio 5 projetos em 5 dias - Tayse Rosa

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,32 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 
+//Components
+import { ListItem } from './components/ListItem';
+ }
+
 const App = () => {
 
   const [ list, setList ] = useState<Item[]>([
     { id: 1, name: "Estudar React JS", done: true },
-    { id: 1, name: "Estudar React Native", done: false },
+    { id: 2, name: "Estudar React Native", done: false },
+    { id: 3, name: "Fazer Café", done: true },
+    { id: 4, name: "Comprar pão e frios na padaria", done: false },
   ]);
 
   return(
     <C.Container>
       <C.Area> 
-        <C.Header> Lista de Tarefas </C.Header>  
-
-        { /* Area de adicionar nova tarefa */ }
+        <C.Header> Tayse's toDo List </C.Header>  
 
         
+
+        { list.map(( item, index )=> (
+          <ListItem 
+            key={index}
+            item={item} 
+          />
+        ))}
 
       </C.Area>
     </C.Container>
