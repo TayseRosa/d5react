2657503e0c5c9926f00cf560e5e272af40cebe46
Tayse <tayse_rosa@yahoo.com.br>
2021-10-05 10:31:59 -0300

    React JS Desafio 5 projetos em 5 dias - Tayse Rosa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
 
-  const [ list, setList ] = useState([
-    
+  const [ list, setList ] = useState<Item[]>([
+    { id: 1, name: "Estudar React JS", done: true },
+    { id: 1, name: "Estudar React Native", done: false },
   ]);
 
   return(
@@ -12,7 +14,9 @@ const App = () => {
       <C.Area> 
         <C.Header> Lista de Tarefas </C.Header>  
 
+        { /* Area de adicionar nova tarefa */ }
 
+        
 
       </C.Area>
     </C.Container>
